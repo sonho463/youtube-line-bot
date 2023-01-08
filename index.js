f97925f82fa2f7466e2472dd500b08f7579ac081
Son Hong Su <son.ho463@gmail.com>
2023-01-08 23:37:18 +0900

    First

---
+++ index.js
@@ -0,0 +1,22 @@
+const line = require("@line/bot-sdk");
+const express = require("express");
+require("dotenv").config();
+
+const CONFIG = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET_KEY,
+};
+const PORT = 8080;
+const client = new line.Client(CONFIG)
+
+express()
+  .post("/webhook", line.middleware(CONFIG), (req, res) => handleBot(req, res))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function handleBot(req, res) {
+  res.status(200).end();
+  req.body.events.map((event) => {
+		console.log('event', event)
+		client.replyMessage(event.replyToken,{type:'text',text:'こんにちは'})
+  });
+}
